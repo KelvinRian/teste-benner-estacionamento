a32f19ce888eb767d8a56e397530f7d7c23380c7
Kelvin Rian <kelvinlourenco18@gmail.com>
2025-07-14 19:39:14 -0300

    implemented entities methods

--- ParkingManagement.Api/ParkingManagement.Domain/Entities/ParkingSession.cs
+++ ParkingManagement.Api/ParkingManagement.Domain/Entities/ParkingSession.cs
@@ -1,11 +1,27 @@
-﻿namespace ParkingManagement.Domain.Entities
+﻿using ParkingManagement.Domain.Commands;
+
+namespace ParkingManagement.Domain.Entities
 {
     public class ParkingSession : BaseEntity
     {
         public string LicensePlate { get; set; }
         public DateTime EntryTime { get; set; }
-        public DateTime ExitTime { get; set; }
+        public DateTime? ExitTime { get; set; }
         public Guid PriceId { get; set; }
         public Price Price { get; set; }
+
+        private ParkingSession() { }
+
+        public ParkingSession(EntryCommand entryCommand, Guid priceId)
+        {
+            LicensePlate = entryCommand.LicensePlate;
+            EntryTime = DateTime.UtcNow;
+            PriceId = priceId;
+        }
+
+        public void Exit()
+        {
+            ExitTime = DateTime.UtcNow;
+        }
     }
 }
---
+++ ParkingManagement.Api/ParkingManagement.Tests/Domain/ParkingSessionTests.cs
@@ -0,0 +1,43 @@
+﻿using ParkingManagement.Domain.Commands;
+using ParkingManagement.Domain.Entities;
+
+namespace ParkingManagement.Tests.Domain
+{
+    public class ParkingSessionTests
+    {
+        [Fact]
+        public void shouldConstruct()
+        {
+            var command = new EntryCommand()
+            {
+                LicensePlate = "ABC-1234"
+            };
+
+            var priceId = Guid.NewGuid();
+
+            var result = new ParkingSession(command, priceId);
+
+            Assert.Equal(command.LicensePlate, result.LicensePlate);
+            Assert.InRange(result.EntryTime, DateTime.UtcNow.AddSeconds(-59), DateTime.UtcNow.AddSeconds(59));
+            Assert.Equal(DateTimeKind.Utc, result.EntryTime.Kind);
+            Assert.Null(result.ExitTime);
+            Assert.Equal(priceId, result.PriceId);
+            Assert.Null(result.Price);
+        }
+
+        [Fact]
+        public void shouldExit()
+        {
+            var command = new EntryCommand();
+            var priceId = Guid.NewGuid();
+
+            var parkingSession = new ParkingSession(command, priceId);
+
+            parkingSession.Exit();
+
+            Assert.NotNull(parkingSession.ExitTime);
+            Assert.InRange(parkingSession.ExitTime.Value, DateTime.UtcNow.AddSeconds(-59), DateTime.UtcNow.AddSeconds(59));
+            Assert.Equal(DateTimeKind.Utc, parkingSession.ExitTime.Value.Kind);
+        }
+    }
+}
