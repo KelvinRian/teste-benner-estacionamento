a41f94afcbf63a7d5edbc5277d5736de1b89ae98
Kelvin Rian <kelvinlourenco18@gmail.com>
2025-07-14 02:12:21 -0300

    add entities

---
+++ ParkingManagement.Api/ParkingManagement.Domain/Entities/ParkingSession.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkingManagement.Domain.Entities
+{
+    public class ParkingSession : BaseEntity
+    {
+        public string LicensePlate { get; set; }
+        public DateTime EntryTime { get; set; }
+        public DateTime ExitTime { get; set; }
+        public Price Price { get; set; }
+    }
+}
---
+++ ParkingManagement.Api/ParkingManagement.Domain/Entities/Price.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkingManagement.Domain.Entities
+{
+    public class Price : BaseEntity
+    {
+        public decimal BaseValue { get; set; }
+        public decimal ExtraTimeValue { get; set; }
+        public DateTime EffectivePeriodStart { get; set; }
+        public DateTime EffectivePeriodEnd { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
