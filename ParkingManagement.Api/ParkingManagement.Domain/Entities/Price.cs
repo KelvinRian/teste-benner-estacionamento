fc7e74e1bcd8f2611450ebf4e39a17d2d83c151f
Kelvin Rian <kelvinlourenco18@gmail.com>
2025-07-14 17:53:16 -0300

    add tests project

--- ParkingManagement.Api/ParkingManagement.Domain/Entities/Price.cs
+++ ParkingManagement.Api/ParkingManagement.Domain/Entities/Price.cs
@@ -1,11 +1,24 @@
-﻿namespace ParkingManagement.Domain.Entities
+﻿using ParkingManagement.Domain.Commands;
+
+namespace ParkingManagement.Domain.Entities
 {
     public class Price : BaseEntity
     {
-        public decimal BaseValue { get; set; }
-        public decimal ExtraTimeValue { get; set; }
-        public DateTime EffectivePeriodStart { get; set; }
-        public DateTime EffectivePeriodEnd { get; set; }
-        public DateTime CreationDate { get; set; }
+        public decimal BaseValue { get; private set; }
+        public decimal ExtraTimeValue { get; private set; }
+        public DateTime EffectivePeriodStart { get; private set; }
+        public DateTime EffectivePeriodEnd { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        private Price() { }
+
+        public Price(CreatePriceCommand createPriceCommand)
+        {
+            BaseValue = createPriceCommand.BaseValue;
+            ExtraTimeValue = createPriceCommand.ExtraTimeValue;
+            EffectivePeriodStart = createPriceCommand.EffectivePeriodStart;
+            EffectivePeriodEnd = createPriceCommand.EffectivePeriodEnd;
+            CreationDate = DateTime.UtcNow;
+        }
     }
 }
---
+++ ParkingManagement.Api/ParkingManagement.Tests/Domain/PriceTests.cs
@@ -0,0 +1,33 @@
+﻿using ParkingManagement.Domain.Commands;
+using ParkingManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingManagement.Tests.Domain
+{
+    public class PriceTests
+    {
+        [Fact]
+        public void shouldConstruct()
+        {
+            var command = new CreatePriceCommand()
+            {
+                BaseValue = 10,
+                ExtraTimeValue = 5,
+                EffectivePeriodStart = DateTime.Now.AddDays(-10),
+                EffectivePeriodEnd = DateTime.Now,
+            };
+
+            var result = new Price(command);
+
+            Assert.Equal(command.BaseValue, result.BaseValue);
+            Assert.Equal(command.ExtraTimeValue, result.ExtraTimeValue);
+            Assert.Equal(command.EffectivePeriodStart, result.EffectivePeriodStart);
+            Assert.Equal(command.EffectivePeriodEnd, result.EffectivePeriodEnd);
+            Assert.InRange(result.CreationDate, DateTime.UtcNow.AddMinutes(-2), DateTime.UtcNow.AddMinutes(2));
+        }
+    }
+}
